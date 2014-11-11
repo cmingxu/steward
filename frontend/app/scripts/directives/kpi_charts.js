bbba7d9401bd4a066d150b68a3dd4f9e7f2dd84b
Jasper Fu <sword0607@gmail.com>
2014-11-11 18:14:01 +0800

    add charts directive

---
+++ frontend/app/scripts/directives/kpi_charts.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontendApp.directive:charts
+ * @description
+ * # charts
+ */
+ angular.module('frontendApp')
+ .directive('kpiCharts', function () {
+  return {
+    templateUrl: 'views/templates/kpi_charts.html',
+    restrict: 'E',
+    scope: {
+      kpiData: '=',
+      kpiChartType: '='
+    }
+  };
+});
